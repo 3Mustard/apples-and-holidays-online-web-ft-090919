861b5ce7c68be52c7b2cc7542bba050933bb5fa8
Matthew C <m23.cullen@gmail.com>
2019-09-13 13:14:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season|
+  holiday_hash.each do |season,holiday|
     puts "#{season}:"
     season.each do |holiday,supply|
       puts "#{holiday}: " + supply.split(",")
